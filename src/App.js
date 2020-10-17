51cadc539e013513add3505426f51608c59dfbf7
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-17 15:12:06 +0300

    Feat: Add husky and lint-staged

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 import React from 'react';
-
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl text-red-300">Hello World</h1>
+      <p>learn react</p>
     </div>
   );
 }
