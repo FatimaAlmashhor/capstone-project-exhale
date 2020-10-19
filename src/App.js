32e642c7a0c06dd2e5466130c93a7368cc970e30
pepeep <noha.pepeep1@gmail.com>
2020-10-19 22:55:36 +0300

    Fix : Rebase to latest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 /* eslint-disable */
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
 function App() {
-  // or const [t, i18n] = useTranslation();
-  const { t, i18n } = useTranslation();
-
-  function handleLang(lang) {
-
-    i18n.changeLanguage(lang);
-  }
-  return (
-    <div>
-      <button className="bg-blue mr-10" onClick={() => handleLang('ar')}>
-        Arabic{' '}
-      </button>
-      <button
-        className="bg-blue-400 mr-10 from-red-800"
-        onClick={() => handleLang('en')}
-      >
-        English{' '}
-      </button>
-      <h1 className="text-3xl text-red-300">{t('Hello.1')}</h1>
-      <p>text</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
