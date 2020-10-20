658a07425cdcda91e2e41537688e737e4c67bccd
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-10-20 12:20:02 +0300

    Doc: add fake data file add a js file tat contains 5 doctors info and 5 articles.

---
+++ fakeData.js
@@ -0,0 +1,88 @@
+const psychiarists = [
+    {
+       name: "Dr. Nabeel No'man",
+        specialtyies: "Anxiety, Bipolar Disorder, Depression, General Adult Psychiatry, Personality Disorders",
+        workplace: "Azal Hospital",
+        address: "60th street",
+        email: "example@gmail.com"
+        phone: "+967770000000"
+    },
+{
+       name: "Dr. Nabeel No'man",
+        specialtyies: "Anxiety, Bipolar Disorder, Depression, General Adult Psychiatry, Personality Disorders",
+        workplace: "Azal Hospital",
+        address: "60th street",
+        email: "example@gmail.com"
+        phone: "+967770000000"
+    },
+  {
+       name: "Dr. Nabeel No'man",
+        specialtyies: "Anxiety, Bipolar Disorder, Depression, General Adult Psychiatry, Personality Disorders",
+        workplace: "Azal Hospital",
+        address: "60th street",
+        email: "example@gmail.com"
+        phone: "+967770000000"
+    },
+  {
+       name: "Dr. Nabeel No'man",
+        specialtyies: "Anxiety, Bipolar Disorder, Depression, General Adult Psychiatry, Personality Disorders",
+        workplace: "Azal Hospital",
+        address: "60th street",
+        email: "example@gmail.com"
+        phone: "+967770000000"
+    },
+  {
+       name: "Dr. Nabeel No'man",
+        specialtyies: "Anxiety, Bipolar Disorder, Depression, General Adult Psychiatry, Personality Disorders",
+        workplace: "Azal Hospital",
+        address: "60th street",
+        email: "example@gmail.com"
+        phone: "+967770000000"
+    }
+];
+const articles = [
+    {
+        title: "What is depression",
+        articleImg: "https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg",
+        author: "Ali",
+        authorDetails: "whatever whatever",
+        authorImg: "https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg",
+        text: "Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. "
+    },
+    {
+        title: "What is depression",
+        articleImg: "https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg",
+        author: "Ali",
+        authorDetails: "whatever whatever",
+        authorImg: "https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg",
+        text: "Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. "
+    },
+     {
+        title: "What is depression",
+        articleImg: "https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg",
+        author: "Ali",
+        authorDetails: "whatever whatever",
+        authorImg: "https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg",
+        text: "Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. "
+    },
+     {
+        title: "What is depression",
+        articleImg: "https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg",
+        author: "Ali",
+        authorDetails: "whatever whatever",
+        authorImg: "https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg",
+        text: "Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. "
+    },
+     {
+        title: "What is depression",
+        articleImg: "https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg",
+        author: "Ali",
+        authorDetails: "whatever whatever",
+        authorImg: "https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg",
+        text: "Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. "
+    } 
+];
+module.exports = {
+    psychiarists,
+    articles
+};
