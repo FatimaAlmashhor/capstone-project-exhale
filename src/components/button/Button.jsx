d0c2759df8c8680d7247ea3a4865e6690d5608cd
Fatima Amin <fatima.amin4official@gmail.com>
2020-10-21 18:44:24 +0300

    Perf : change button to defaultProps

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,22 +5,35 @@ export default function Button({ submit, height, width, color, hover, textColor 
   const [style, setStyle] = useState('');
   useEffect(() => {
     setStyle(
-      `text-${textColor} bg-${color} hover:bg-${hover} border  hover:border-${color}  font-bold py-2 px-3 w-${width} h-${height} transition duration-500  ease-in-out rounded`
+      `text-${textColor} 
+       bg-${color} 
+       hover:bg-${hover} 
+       border  hover:border-${color}  
+       font-bold py-2 px-3 w-${width} 
+       h-${height} 
+       transition duration-500  ease-in-out rounded`
     );
   }, []);
   return (
-    <div>
-      <button type={submit ? 'submit' : 'button'} className={style}>
-        Button
-      </button>
-    </div>
+    <button type={submit ? 'submit' : 'button'} className={style}>
+      Button
+    </button>
   );
 }
 Button.propTypes = {
-  submit: PropTypes.bool.isRequired,
-  height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
-  color: PropTypes.string.isRequired,
-  textColor: PropTypes.string.isRequired,
-  hover: PropTypes.string.isRequired,
+  submit: PropTypes.bool,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  color: PropTypes.string,
+  textColor: PropTypes.string,
+  hover: PropTypes.string,
+};
+
+Button.defaultProps = {
+  submit: false,
+  height: 30,
+  width: 40,
+  color: 'mainPink',
+  textColor: 'black',
+  hover: 'transparent',
 };
