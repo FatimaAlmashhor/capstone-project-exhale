a095108c6379baaac4602bb3ebb79fe2b88440b5
pepeep <noha.pepeep1@gmail.com>
2020-10-21 12:17:39 +0300

    Build : Add Font Awesome
    Taking fontawesome to the next level

---
+++ src/fontawesome.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far} from "@fortawesome/free-regular-svg-icons";
+
+library.add(
+  fas,far
+  // more icons go here
+);
