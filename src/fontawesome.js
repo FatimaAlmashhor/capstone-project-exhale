937ad6aaf3fe0afc297ffc0e69c84d3daec80243
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-22 16:55:32 +0300

    Feat: import needed icons only

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,12 +1,11 @@
-/* eslint-disable */
-// import the library
 import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
-// import your icons
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far} from "@fortawesome/free-regular-svg-icons";
+library.add(faTwitter, faFacebook, faLinkedin, faInstagram);
 
-library.add(
-  fas,far
-  // more icons go here
-);
+// Import example:<FontAwesomeIcon icon={['fab', 'twitter']} />
