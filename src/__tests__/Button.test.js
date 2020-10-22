5dfee0a83adee307dfc4064966e3806c4787e26d
Fatima Amin <fatima.amin4official@gmail.com>
2020-10-22 19:52:35 +0300

    Revert "Test : fix button folder name"
    This reverts commit 654384f3e2827242116817604ec121528ebb3682.

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Button from '../components/Button';
+import Button from '../components/Button/index';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Button />).toJSON();
