e88e772c76de24bc5f48caa894e153ae7257f503
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-10-22 11:37:19 +0300

    Fix: rebase to the branch develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 /* eslint-disable */
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
+//Edit API key and authDomain from firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyCIsm5BgxDnP1xWnUL0sz5YRtCBU7uPHvI",
+  authDomain: "exhale-mental-health.firebaseapp.com"
+})
+ 
 function App() {
   // or const [t, i18n] = useTranslation();
   const { t, i18n } = useTranslation();
@@ -10,6 +19,24 @@ function App() {
 
     i18n.changeLanguage(lang);
   }
+  //Firebase
+  const [isSignedIn, setisSignedIn] = useState(false);
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => console.log("Success")
+    }
+  };
+  useEffect(() => {
+   firebase.auth().onAuthStateChanged(user => {
+      setisSignedIn(!!user)
+      console.log("user", user)
+   })
+  });
   return (
     <div>
       <button className="bg-blue mr-10" onClick={() => handleLang('ar')}>
@@ -23,8 +50,25 @@ function App() {
       </button>
       <h1 className="text-3xl text-red-300">{t('Hello.1')}</h1>
       <p>text</p>
+       {/*Firebase Test*/}
+       {isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth 
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
     </div>
-  );
+  )
 }
 
 export default App;
