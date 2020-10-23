20752b6b751d6ef66e02aba1e4e9ec29a2396ea4
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-10-23 19:11:42 +0300

    Fix:fix proptypes for test

---
+++ src/Input/Input.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Formik } from 'formik';
+import PropType from 'prop-types';
+
+function Input(props) {
+  const { handleSubmit, handleChange, name, errors, labelText, type, value } = props;
+  return (
+    <Formik>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={name}>{labelText}</label>
+        <input type={type} onChange={handleChange} name={name} value={value} />
+        {errors && <div className="input-feedback">{errors}</div>}
+      </form>
+    </Formik>
+  );
+}
+
+Input.propTypes = {
+  handleSubmit: PropType.string.isRequired,
+  handleChange: PropType.string.isRequired,
+  name: PropType.string.isRequired,
+  errors: PropType.string.isRequired,
+  labelText: PropType.string.isRequired,
+  type: PropType.string.isRequired,
+  value: PropType.string.isRequired,
+};
+export default Input;
