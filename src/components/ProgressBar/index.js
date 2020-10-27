c1eafb342243ce1ef7c49baaa82b001994f6ed86
pepeep <noha.pepeep1@gmail.com>
2020-10-27 19:19:21 +0300

    Test : add test of progressbar component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,1 +1,11 @@
 export { default } from './ProgressBar';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Progress from './ProgressBar';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Progress />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
