537866caac5a77b368cc02b11e3ca46fb011cffe
pepeep <noha.pepeep1@gmail.com>
2020-10-27 21:46:25 +0300

    Test : repair the test for the last time I hope

--- src/__test__/ProgressBar/ProgressBar.test.js
+++ src/__test__/ProgressBar/ProgressBar.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Progress from '../../components/progressBar/ProgressBar';
+import ProgressBar from '../../components/progressBar/ProgressBar';
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(<Progress />)
-    .toJSON();
+  const tree = renderer.create(<ProgressBar />).toJSON();
   expect(tree).toMatchSnapshot();
 });
