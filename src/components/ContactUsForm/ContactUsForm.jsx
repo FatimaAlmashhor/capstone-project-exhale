0b89928791edfccd251fe85079391d12b105b55d
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-10-27 03:23:20 +0300

    Feat: add contactUs with forimk

---
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const ReviewError = Yup.object().shape({
+  email: Yup.string().email().required('Email is Required'),
+  message: Yup.string().required('Message is Required'),
+});
+const ContactUsForm = () => (
+  <Formik initialValues={{ email: '', message: '' }} validationSchema={ReviewError}>
+    {(props) => {
+      const { errors, handleChange, handleSubmit } = props;
+      return (
+        <div className="w-full max-w-xs  mx-auto">
+          <h2 className="text-center mt-5">GET IN TOUCH </h2>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="text"
+              placeholder="Enter your Email"
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <div className="text-red-500 text-sm font-bold">{errors.email}</div>
+            )}
+            <br />
+            <label
+              htmlFor="message"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Message
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="message"
+              name="message"
+              rows="5"
+              placeholder="Enter your Message"
+              onChange={handleChange}
+            />
+            {errors.message && (
+              <div className="text-red-500 text-sm font-bold">{errors.message}</div>
+            )}
+
+            <button
+              className="bg-red-500 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              SEND
+            </button>
+          </form>
+        </div>
+      );
+    }}
+  </Formik>
+);
+
+export default ContactUsForm;
