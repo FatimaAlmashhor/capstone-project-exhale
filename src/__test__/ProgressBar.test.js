b4ba7fc58e9224a37a7f155ac19d8d0305d43dcf
pepeep <noha.pepeep1@gmail.com>
2020-10-27 20:39:15 +0300

    Test: repair progressbar test

--- src/__test__/ProgressBar.test.js
+++ src/__test__/ProgressBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Progress from '../../components/ProgressBar/ProgressBar';
+import Progress from '../../components/progressBar/ProgressBar';
 
 it('renders correctly', () => {
   const tree = renderer
