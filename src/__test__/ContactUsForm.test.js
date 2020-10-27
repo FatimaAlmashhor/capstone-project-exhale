750df43c68b52ae385d809500a3b99cd6eecffa1
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-10-27 03:31:31 +0300

    Test: add test for contactus form

---
+++ src/__test__/ContactUsForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ContactUsForm from '../components/ContactUsForm'
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ContactUsForm />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
