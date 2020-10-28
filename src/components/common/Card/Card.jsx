75babbde7182accf4e4d61a92f50771b5ff80f47
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-28 19:01:25 +0300

    Feat: rebase to latest and remove state from card

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -1,19 +1,14 @@
-import React  , {useState} from 'react'
-import PropTypes from 'prop-types' ;
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Card({children }) {
-    const [hover , setHover] = useState(' ');
-    return (
-        <div 
-            className = {`max-w-screen-sm md:max-w-xs lg:max-w-xs rounded overflow-hidden border ${hover}`}
-            onMouseEnter = {() => setHover('shadow-md')}    
-            onMouseLeave = {() => setHover(' ')}    
-        >
-            {children}
-        </div>
-    )
+export default function Card({ children }) {
+  return (
+    <div className="max-w-screen-sm md:max-w-xs lg:max-w-xs rounded overflow-hidden border hover:shadow-md">
+      {children}
+    </div>
+  );
 }
 
 Card.propTypes = {
-    children : PropTypes.element.isRequired 
-} 
+  children: PropTypes.element.isRequired,
+};
