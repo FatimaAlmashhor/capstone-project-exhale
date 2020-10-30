9b9889ce3f60931e890e39220da0890b7790ac4b
Fatima Amin <fatima.amin4official@gmail.com>
2020-10-30 19:28:22 +0200

    Feat : Add ArticalCard component

--- src/__test__/ArticleCard.test.js
+++ src/__test__/ArticleCard.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+<<<<<<< HEAD
 import Article from '../components/ArticleCard';
 it('renders correctly', () => {
    let fun = function() {};
   const tree = renderer
     .create(<Article title = {' '} paragraph = {' '} onClick = {fun}/>)
+=======
+import Card from '../components/ArticleCard';
+
+it('artical card', () => {
+  const tree = renderer
+    .create(<Card header = {' '} paragraph = {' '}/>)
+>>>>>>> 39e4212... Feat : Add ArticalCard component
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+<<<<<<< HEAD
  const ArticleCard = ({ title, paragraph, time , img , onClick}) => {
   let handleCLick = () => {
     // on click code
@@ -43,6 +44,34 @@ ArticleCard.propTypes = {
   onClick : PropTypes.func.isRequired,
   time: PropTypes.string,
   img : PropTypes.string,
+=======
+// import Button from '../common/Button' ;
+
+export default function ArticleCard({ header, paragraph, time }) {
+  return (
+    <div className="max-w-screen-sm md:max-w-sm lg:max-w-sm rounded overflow-hidden ">
+      <div className="px-6 pt-4 pb-2 text-gray-500">{time}</div>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-5">{header}</div>
+        <p className="text-gray-700 text-base">{paragraph}</p>
+      </div>
+      <div className="px-6 pt-3 pb-5 mt-10">
+        {/* <Button text=''/> */}
+        <button
+          type="button"
+          className=" border py-2 px-3 text-white hover:border-1 hover: border-green-400 hover:text-green-400 hover:bg-transparent rounded bg-green-400"
+        >
+          Read Full
+        </button>
+      </div>
+    </div>
+  );
+}
+ArticleCard.propTypes = {
+  header: PropTypes.string.isRequired,
+  paragraph: PropTypes.string.isRequired,
+  time: PropTypes.string,
+>>>>>>> 39e4212... Feat : Add ArticalCard component
 };
 ArticleCard.defaultProps = {
   time: new Date().toLocaleDateString('en-US'),
