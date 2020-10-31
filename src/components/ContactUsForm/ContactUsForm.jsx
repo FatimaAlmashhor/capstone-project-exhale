f4edd91bafa1311602fb880edc9345b6c6d703e5
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-11-01 02:10:32 +0300

    Fix: fix error eslint

--- src/components/ContactUsForm/ContactUsForm.jsx
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -2,55 +2,63 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
- export default function ContactUsForm  () {
-     const {t} = useTranslation();
-     const ReviewError = Yup.object().shape({
-      email: Yup.string().email().required('Email is Required'),
-      message: Yup.string().required('Message is Required')
-       });
-      return (
-        <Formik initialValues={{ email: '', message: '' }} 
-        validationSchema={ReviewError}>
-        {(formik) =>(
-         <div className="w-full max-w-xs  mx-auto">
-           <form
+
+export default function ContactUsForm() {
+  const { t } = useTranslation();
+  const ReviewError = Yup.object().shape({
+    email: Yup.string().email().required('Email is Required'),
+    message: Yup.string().required('Message is Required'),
+  });
+  return (
+    <Formik
+      initialValues={{ email: '', message: '' }}
+      validationSchema={ReviewError}
+    >
+      {(formik) => (
+        <div className="w-full max-w-xs  mx-auto">
+          <form
             onSubmit={formik.handleSubmit}
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
             <label
               htmlFor="email"
               className="block text-gray-700 text-sm font-bold mb-2"
             >
-            {t('Email')}
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              name="email"
-              type="text"
-              placeholder="Enter your Email"
-              onChange={formik.handleChange}
-            />
+              {t('Email')}
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Enter your Email"
+                onChange={formik.handleChange}
+              />
             </label>
-            {  formik.errors.email &&  (
-              <div className="text-red-500 text-sm font-bold">{formik.errors.email}</div>
+            {formik.errors.email && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.email}
+              </div>
             )}
-            <br />
+
             <label
               htmlFor="message"
               className="block text-gray-700 text-sm font-bold mb-2"
             >
-            {t('Message')}
-          
-            <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="message"
-              name="message"
-              rows="5"
-              placeholder="Enter your Message"
-              onChange={formik.handleChange}
-            />
+              {t('Message')}
+
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="message"
+                name="message"
+                rows="5"
+                placeholder="Enter your Message"
+                onChange={formik.handleChange}
+              />
             </label>
-            { formik.errors.message  && (
-              <div className="text-red-500 text-sm font-bold">{formik.errors.message}</div>
+            {formik.errors.message && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.message}
+              </div>
             )}
             <button
               className="bg-red-500 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -58,9 +66,9 @@ import { useTranslation } from 'react-i18next';
             >
               SEND
             </button>
-           </form>
-          </div>
-        )}
-        </Formik>
-      );
-  }
+          </form>
+        </div>
+      )}
+    </Formik>
+  );
+}
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,57 +2,73 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
+
 export default function RegisterForm() {
-        const {t} = useTranslation();
-        const ReviewError=Yup.object().shape({
-         name: Yup.string().required('Name is Required '),
-         email: Yup.string().email().required('Email is Required'),
-         password: Yup.string()
-          .min(8, 'Password must be at least 8 characters')
-          .required('Password is Required'),
-         confirmPassword: Yup.string()
-          .oneOf([Yup.ref('password'), null], 'Passwords must match')
-          .required('Confirm Password is Required')
-         });
-      return (   
-        <Formik initialValues={{ name: '', email: '', password: '', confirmPassword: '' }}
-          validationSchema={ReviewError}>
-          {(formik) =>(
+  const { t } = useTranslation();
+  const ReviewError = Yup.object().shape({
+    name: Yup.string().required('Name is Required '),
+    email: Yup.string().email().required('Email is Required'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Password is Required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Confirm Password is Required'),
+  });
+  return (
+    <Formik
+      initialValues={{ name: '', email: '', password: '', confirmPassword: '' }}
+      validationSchema={ReviewError}
+    >
+      {(formik) => (
         <div className="w-full max-w-xs mx-auto">
-          <form onSubmit={formik.handleSubmit} className="bg-white shadow-md  rounded px-8 pt-6 pb-8 mb-4">
-            <label htmlFor="name"className="block text-gray-700 text-sm font-bold mb-2">
-               {t('Name')}
-               <input
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white shadow-md  rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <label
+              htmlFor="name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              {t('Name')}
+              <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 name="name"
                 type="text"
                 placeholder="Enter your Name"
                 onChange={formik.handleChange}
-               />
+              />
             </label>
-              {formik.errors.name && (<div className="text-red-500 text-sm font-bold">
-                {formik.errors.name}</div> )}
-              <label
-               htmlFor="email"
-               className="block text-gray-700 text-sm font-bold mb-2">
-               {t('Email')}
-               <input
+            {formik.errors.name && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.name}
+              </div>
+            )}
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              {t('Email')}
+              <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
                 name="email"
                 type="text"
                 placeholder="Enter your email"
                 onChange={formik.handleChange}
-                />
-              </label>
-                {formik.errors.email &&
-                 (<div className="text-red-500 text-sm font-bold">{formik.errors.email}</div>
-                )}
-              <label
-                htmlFor="password"
-                className="block text-gray-700 text-sm font-bold mb-2">
-                {t('Password')}
+              />
+            </label>
+            {formik.errors.email && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.email}
+              </div>
+            )}
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              {t('Password')}
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
@@ -60,15 +76,18 @@ export default function RegisterForm() {
                 type="password"
                 placeholder="Enter your password"
                 onChange={formik.handleChange}
-               />
-              </label>
-              {formik.errors.password && 
-                ( <div className="text-red-500 text-sm font-bold">{formik.errors.password}</div>
-              )}
-             <label
-                htmlFor="confirmPassword"
-                className="block text-gray-700 text-sm font-bold mb-2">
-                {t('confirmPassword')}
+              />
+            </label>
+            {formik.errors.password && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.password}
+              </div>
+            )}
+            <label
+              htmlFor="confirmPassword"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              {t('confirmPassword')}
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="confirmPassword"
@@ -77,18 +96,21 @@ export default function RegisterForm() {
                 placeholder="Confirm your password"
                 onChange={formik.handleChange}
               />
-             </label>
-              {formik.errors.confirmPassword && 
-                ( <div className="text-red-500 text-sm font-bold">{formik.errors.confirmPassword} </div>
-               )}
-              <button
-                className="bg-red-500 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit">
-                Sign UP
-              </button>
+            </label>
+            {formik.errors.confirmPassword && (
+              <div className="text-red-500 text-sm font-bold">
+                {formik.errors.confirmPassword}
+              </div>
+            )}
+            <button
+              className="bg-red-500 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign UP
+            </button>
           </form>
         </div>
-        )}
-      </Formik>
-      );
-  }
+      )}
+    </Formik>
+  );
+}
