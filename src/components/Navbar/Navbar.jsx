9b1ff901bf3f403ccb98cce446e909a8a891fa24
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-11-01 21:28:46 +0300

    Feat: edite color for nav items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,79 +14,55 @@ import {
 function Navbar() {
   const [isExpanded, toggleExpansion] = useState(false);
   return (
-    // bg-gray-800
     <div className="relative bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="lg:w-0 lg:flex-1">
             <Link to={HOME_ROUTE} class="flex">
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg"
-                alt="Workflow"
-              />
+              <h1 className="font-medium text-3xl text-black-500 transition ease-in-out duration-150">
+                <span className="text-4xl text-blue-800 font-black">EX</span>
+                hale
+              </h1>
             </Link>
-          </div>
-          <div className="-mr-2 -my-2 md:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-            >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
           </div>
           <nav className="hidden md:flex space-x-10">
             <NavLink
               to={HOME_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-blue-900 hover:text-gray-500 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               HOME
             </NavLink>
-
             <NavLink
               to={DOCTORS_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               Doctors
             </NavLink>
 
             <NavLink
               to={ARTICLES_BASE_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               Articles
             </NavLink>
 
             <NavLink
               to={ABOUT_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               AboutUs
             </NavLink>
 
             <NavLink
               to={CONTACT_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               ContactUs
             </NavLink>
 
             <NavLink
               to={SELF_ASSESSMENT_ROUTE}
-              class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
+              class="text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
             >
               Checkup
             </NavLink>
@@ -94,7 +70,7 @@ function Navbar() {
           <div className="hidden md:flex items-center justify-end space-x-8 md:flex-1 lg:w-0">
             <NavLink
               to={LOGIN_ROUTE}
-              class="whitespace-no-wrap text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900"
+              class="whitespace-no-wrap text-base leading-6 font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900"
             >
               Login
             </NavLink>
