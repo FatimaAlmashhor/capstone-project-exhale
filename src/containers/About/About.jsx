66d997acc98f4deffccfd9a64d92389cdc9f4202
Dhiaa Alshamy <dhodhof@gmail.com>
2020-11-02 22:06:11 +0300

    container: about us header and paragraph

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -4,6 +4,14 @@ import TeamCard from '../../components/teamCard';
 const About = () => {
   return (
     <div>
+      <div className="text-center">
+        <h2 className="text-4xl lg:text-5xl font-bold leading-tight">About Us!</h2>
+        <p className="font-mono">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus ex
+          placeat quia id consequuntur ullam, beatae minima sint consectetur natus
+          illo praesentium voluptas maiores! Est ipsam autem eius velit quasi!
+        </p>
+      </div>
       <div className="md:flex md:flex-wrap -mb-4">
         <div className="mb-4 bg-gray-400 sm:w-1  md:w-1/3 ">
           <TeamCard />
