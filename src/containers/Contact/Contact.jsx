2becf01a7324f1ba00c35fb887c76fed1b585235
Dhiaa Alshamy <dhodhof@gmail.com>
2020-11-02 21:00:12 +0300

    build:containers:add responsive team cards

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import TeamCard from '../../components/teamCard';
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <div className="md:flex md:flex-wrap -mb-4">
+        <div className="mb-4 bg-gray-400 sm:w-1  md:w-1/3 ">
+          <TeamCard />
+        </div>
+        <div className="mb-4 bg-gray-500 sm:w-1  md:w-1/3 ">
+          <TeamCard />
+        </div>
+        <div className="mb-4 bg-gray-400 sm:w-1  md:w-1/3 ">
+          <TeamCard />
+        </div>
+      </div>
     </div>
   );
 };
