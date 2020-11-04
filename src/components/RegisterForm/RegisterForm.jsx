931ae7b7a32d8769da13227e7e246f1c2e2f5535
Fatima Amin <fatima.amin4official@gmail.com>
2020-11-04 22:02:32 +0300

    Refactor : Connect signup with database

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
+import { signup } from '../../database/SendDataToDB';
 
 export default function RegisterForm() {
   const { t } = useTranslation();
@@ -13,65 +14,63 @@ export default function RegisterForm() {
       .oneOf([Yup.ref('password'), null], t('MuchPass'))
       .required(t('confirmPasswordRequierd')),
   });
+  const handleSubmit = (values) => {
+    signup(values.email, values.password, values.name);
+  };
   return (
     <Formik
       initialValues={{ name: '', email: '', password: '', confirmPassword: '' }}
       validationSchema={ReviewError}
+      onSubmit={handleSubmit}
     >
       {(formik) => (
         <div className="w-full max-w-md mx-auto">
-          <form
-            onSubmit={formik.handleSubmit}
-          >
-              <input
-                className="  appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white "
-                id="name"
-                name="name"
-                type="text"
-                placeholder={t('yourNam')}
-                onChange={formik.handleChange}
-              />
+          <form onSubmit={formik.handleSubmit}>
+            <input
+              className="  border rounded w-full py-3 px-3 text-gray-700  focus:outline-none focus:bg-white "
+              id="name"
+              name="name"
+              type="text"
+              placeholder={t('yourNam')}
+              onChange={formik.handleChange}
+            />
             {formik.errors.name && (
-              <div className="text-red-500 text-sm font-bold">
-                {formik.errors.name}
-              </div>
+              <div className=" mt-3 text-red-400 text-sm">{formik.errors.name}</div>
             )}
-              <input
-                className=" mt-8 appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none "
-                id="email"
-                name="email"
-                type="text"
-                placeholder={t('yourEmail')}
-                onChange={formik.handleChange}
-              />
+            <input
+              className=" mt-8  border rounded w-full py-3 px-3 text-gray-700 focus:outline-none "
+              id="email"
+              name="email"
+              type="text"
+              placeholder={t('yourEmail')}
+              onChange={formik.handleChange}
+            />
             {formik.errors.email && (
-              <div className="text-red-500 text-sm font-bold">
-                {formik.errors.email}
-              </div>
+              <div className="mt-3 text-red-400 text-sm">{formik.errors.email}</div>
             )}
-              <input
-                className="mt-8 appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none "
-                id="password"
-                name="password"
-                type="password"
-                placeholder={t('yourPassword')}
-                onChange={formik.handleChange}
-              />
+            <input
+              className="mt-8  border rounded w-full py-3 px-3 text-gray-700  focus:outline-none "
+              id="password"
+              name="password"
+              type="password"
+              placeholder={t('yourPassword')}
+              onChange={formik.handleChange}
+            />
             {formik.errors.password && (
-              <div className="text-red-400 text-sm ">
+              <div className=" mt-3 text-red-400 text-sm ">
                 {formik.errors.password}
               </div>
             )}
-              <input
-                className="mt-8 appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none "
-                id="confirmPassword"
-                name="confirmPassword"
-                type="password"
-                placeholder={t('confirmYourPassword')}
-                onChange={formik.handleChange}
-              />
+            <input
+              className="mt-8  border rounded w-full py-3 px-3 text-gray-700  focus:outline-none "
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              placeholder={t('confirmYourPassword')}
+              onChange={formik.handleChange}
+            />
             {formik.errors.confirmPassword && (
-              <div className="text-red-500 text-sm font-bold">
+              <div className=" mt-3 text-red-400 text-sm">
                 {formik.errors.confirmPassword}
               </div>
             )}
