0819d7959420bad80488faebdeb0d43863904aef
Fatima Amin <fatima.amin4official@gmail.com>
2020-11-04 16:04:09 +0300

    Fix : fix Netlify issue

--- src/__test__/Footer.test.js
+++ src/__test__/Footer.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Footer from '../components/Footer';
+import Footer  from '../components/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter, faFacebook, faLinkedin, faInstagram);
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Footer /> )
+    .create(<Router><Footer /></Router> )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom' ;
+import {
+    ABOUT_ROUTE,
+    CONTACT_ROUTE,
+    SELF_ASSESSMENT_ROUTE,
+  } from '../../routes';
 
 const Footer = () => {
     return (
@@ -12,39 +18,45 @@ const Footer = () => {
                         </p>
                     </div>
                     <div className='inline container z-10' >
-                        <ul className =' flex flex-row '>
-                            <li  className = 'flex-1 text-center text-gray-600'>SELF ASSESSMENT</li>
-                            <li  className = 'flex-1 text-center text-gray-600'>ABOUT US</li>
-                            <li  className = 'flex-1 text-center text-gray-600'>CONTACT US</li>
+                        <ul className ='flex flex-row'>
+                            <li  className = 'flex-1 text-center text-gray-600'>
+                                <Link 
+                                    to = {SELF_ASSESSMENT_ROUTE}>SELF ASSESSMENT</Link>
+                            </li>
+                            <li  className = 'flex-1 text-center text-gray-600'>
+                                <Link to ={ABOUT_ROUTE}>ABOUT US</Link>
+                            </li>
+                            <li  className = 'flex-1 text-center text-gray-600'>
+                                <Link to ={CONTACT_ROUTE}>CONTACT US</Link>
+                            </li>
                         </ul>
                     </div>
                     <div className = 'block flex flex-row mt-8' >
                         <div className = 'flex-1 text-gray-400 text-sm'> 
                             Copyright @Exhale All right Reserved.
                         </div>
-                        <div className = 'flex-auto '> 
-                            <ul className ='flex flex-row-reverse '>
+                        <div className = 'flex-auto'> 
+                            <ul className ='flex flex-row-reverse'>
                                 <li className = 'mx-3 cursor-pointer'>
-                                    <a>
+                                    <a href=''>
                                         <FontAwesomeIcon className ='text-gray-500 text-lg hover:text-green-300' icon={['fab', 'twitter']} />
                                     </a>
                                 </li>
                                 <li className = 'mx-3 cursor-pointer'>
-                                    <a>
+                                    <a href=''>
                                         <FontAwesomeIcon className ='text-gray-500 text-lg hover:text-green-300' icon={['fab', 'facebook']} />
                                     </a>
                                 </li>
                                 <li className = 'mx-3 cursor-pointer'>
-                                    <a>
+                                    <a href=''>
                                         <FontAwesomeIcon className ='text-gray-500 text-lg hover:text-green-300' icon = {['fab' , 'instagram']} />
                                     </a>
                                 </li>
                                 <li className = 'mx-3  cursor-pointer '>
-                                    <a>
+                                    <a href=''>
                                         <FontAwesomeIcon className ='text-gray-500 text-lg hover:text-green-300' icon={['fab', 'linkedin']} />
                                     </a>
                                 </li>
-                                
                             </ul>
                         </div>
                     </div>
