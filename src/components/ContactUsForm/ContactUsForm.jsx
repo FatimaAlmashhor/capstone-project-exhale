73b8e810d0d48e81aed6d684f7f517d1be14a171
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-11-07 02:33:47 +0300

    Feat: design container add design for contact us page

--- src/components/ContactUsForm/ContactUsForm.jsx
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -16,52 +16,55 @@ export default function ContactUsForm() {
     >
       {(formik) => (
         <div className="w-full max-w-xs  mx-auto">
+          <h5 className="text-3xl sm:text-3xl text-center text-gray-700 dark:text-white font-extrabold tracking-tight">
+            Get in touch
+          </h5>
+
+          <p className="text-normal text-lg sm:text-2xl  text-center font-medium text-gray-600 dark:text-gray-400 mt-2">
+            Write in the form to comunicate with us
+          </p>
           <form
             onSubmit={formik.handleSubmit}
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            className="p-6 flex flex-col justify-center"
           >
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              {t('Email')}
+            <div className="flex flex-col">
+              <label htmlFor="email" className="hidden">
+                {t('Email')}
+              </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
                 id="email"
                 name="email"
                 type="text"
                 placeholder={t('yourEmail')}
                 onChange={formik.handleChange}
               />
-            </label>
+            </div>
             {formik.errors.email && (
               <div className="text-red-500 text-sm font-bold">
                 {formik.errors.email}
               </div>
             )}
 
-            <label
-              htmlFor="message"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
+            <label htmlFor="message" className="hidden">
               {t('Message')}
-
-              <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="message"
-                name="message"
-                rows="5"
-                placeholder={t('YourMessage')}
-                onChange={formik.handleChange}
-              />
             </label>
+
+            <textarea
+              className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+              id="message"
+              name="message"
+              rows="5"
+              placeholder={t('YourMessage')}
+              onChange={formik.handleChange}
+            />
             {formik.errors.message && (
               <div className="text-red-500 text-sm font-bold">
                 {formik.errors.message}
               </div>
             )}
             <button
-              className="bg-red-500 mt-4 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-700 mt-4  hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               {t('Send')}
