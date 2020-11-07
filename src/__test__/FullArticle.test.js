30df80b2ab056f83e884ed4488bd66de7e3446ca
Dhiaa Alshamy <dhodhof@gmail.com>
2020-11-07 19:31:14 +0300

    feat: add full article component test

--- src/__test__/FullArticle.test.js
+++ src/__test__/FullArticle.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import '../fontawesome';
-import {BrowserRouter as  Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 import Article from '../components/FullArticle';
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Router><Article id = { 1 } /></Router>)
+    .create(
+      <Router>
+        <Article id={1} />
+      </Router>
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
-})
+});
