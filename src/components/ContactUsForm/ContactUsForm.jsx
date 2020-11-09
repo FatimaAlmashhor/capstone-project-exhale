8a35ef55a6d7d1b03290ed48e8d940ee398656c5
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-11-09 18:29:46 +0300

    Fix:remove label remove label and update form

--- src/components/ContactUsForm/ContactUsForm.jsx
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -30,19 +30,15 @@ export default function ContactUsForm() {
               <div className="lg:w-1/2 md:w-2/3 mx-auto">
                 <div className="flex flex-wrap -m-2">
                   <div className="p-2 w-1/2">
-                    <label
-                      htmlFor="name"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      {t('Name')}
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        onChange={formik.handleChange}
-                        className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                      />
-                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder={t('Name')}
+                      onChange={formik.handleChange}
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+
                     {formik.errors.name && (
                       <div className="text-red-500 text-sm font-bold">
                         {formik.errors.name}
@@ -50,19 +46,15 @@ export default function ContactUsForm() {
                     )}
                   </div>
                   <div className="p-2 w-1/2">
-                    <label
-                      htmlFor="www.email"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      {t('Email')}
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        onChange={formik.handleChange}
-                        className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                      />
-                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder={t('Email')}
+                      onChange={formik.handleChange}
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+
                     {formik.errors.email && (
                       <div className="text-red-500 text-sm font-bold">
                         {formik.errors.email}
@@ -70,18 +62,14 @@ export default function ContactUsForm() {
                     )}
                   </div>
                   <div className="p-2 w-full">
-                    <label
-                      htmlFor="message"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      {t('Message')}
-                      <textarea
-                        id="message"
-                        name="message"
-                        onChange={formik.handleChange}
-                        className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                      />
-                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      placeholder={t('Message')}
+                      onChange={formik.handleChange}
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    />
+
                     {formik.errors.message && (
                       <div className="text-red-500 text-sm font-bold">
                         {formik.errors.message}
@@ -91,7 +79,7 @@ export default function ContactUsForm() {
                   <div className="p-2 w-full">
                     <button
                       type="submit"
-                      className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                      className="mt-5 px-4 py-3 w-full rounded text-white inline-block shadow-lg bg-blue-800 hover:bg-blue-600 focus:bg-blue-700"
                     >
                       {t('Send')}
                     </button>
@@ -99,69 +87,6 @@ export default function ContactUsForm() {
                 </div>
               </div>
             </form>
-
-            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-              <a href="www.com" className="text-indigo-500">
-                example@email.com
-              </a>
-              <p className="leading-normal my-5">
-                49 Smith St.
-                <br />
-                Saint Cloud, MN 56301
-              </p>
-              <span className="inline-flex">
-                <a href="www.com" className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                  </svg>
-                </a>
-                <a href="www.com" className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-                </a>
-                <a href="www.com" className="ml-4 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-                  </svg>
-                </a>
-                <a href="www.com" className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                  </svg>
-                </a>
-              </span>
-            </div>
           </div>
         </section>
       )}
