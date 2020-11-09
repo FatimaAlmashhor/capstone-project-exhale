4d25160b0798d41565acf0050a7cf199966fa53f
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-11-09 18:38:47 +0300

    Feat: add some stye add color for header of ContactUsForm

--- src/components/ContactUsForm/ContactUsForm.jsx
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -19,8 +19,8 @@ export default function ContactUsForm() {
         <section className="text-gray-700 body-font ">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-col text-center w-full mb-12">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-                GET IN TOUCH
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-blue-800">
+                {t('GetInTouch')}
               </h1>
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
                 {t('contactus')}
