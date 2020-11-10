52ad058ec3cc999be2340d5f26e1e3c1ef096f7a
Fatima Amin <fatima.amin4official@gmail.com>
2020-11-10 19:53:51 +0300

    Feat : fill unfound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import unfound from '../../Lottie/404-animation.json';
 
 const NotFound = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: unfound,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
-    <div>
-      <h1>Not Found Page</h1>
+    <div className="w-full h-full ">
+      <div className="container mx-auto w-2/5 my-20 ">
+        <Lottie options={defaultOptions} height="auto" width="100%" />
+      </div>
     </div>
   );
 };
