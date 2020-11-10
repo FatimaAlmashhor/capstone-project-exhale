f862c43b7f4e3c2565006f02fb0452bf35be42bf
Dhiaa Alshamy <dhodhof@gmail.com>
2020-11-11 01:37:02 +0300

    build: add express & scrapp the articles

--- src/services/fakeArticleService.js
+++ src/services/fakeArticleService.js
@@ -1,71 +1,12 @@
-export const articles = [
-  {
-    id: 1,
-    date: 'March 12th, 2019',
-    title: 'What is depression',
-    articleImg:
-      'https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg',
-    author: 'Ali',
-    authorDetails: 'whatever whatever',
-    authorImg:
-      'https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg',
-    text:
-      'Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. ',
-  },
-  {
-    id: 2,
-    date: 'March 12th, 2019',
-    title: 'What is depression',
-    articleImg:
-      'https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg',
-    author: 'Ali',
-    authorDetails: 'whatever whatever',
-    authorImg:
-      'https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg',
-    text:
-      'Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. ',
-  },
-  {
-    id: 3,
-    date: 'March 12th, 2019',
-    title: 'What is depression',
-    articleImg:
-      'https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg',
-    author: 'Ali',
-    authorDetails: 'whatever whatever',
-    authorImg:
-      'https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg',
-    text:
-      'Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. ',
-  },
-  {
-    id: 4,
-    date: 'March 12th, 2019',
-    title: 'What is depression',
-    articleImg:
-      'https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg',
-    author: 'Ali',
-    authorDetails: 'whatever whatever',
-    authorImg:
-      'https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg',
-    text:
-      'Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. ',
-  },
-  {
-    id: 5,
-    date: 'March 12th, 2019',
-    title: 'What is depression',
-    articleImg:
-      'https://media.gq-magazine.co.uk/photos/5d1390269a22c2b6ef9478f3/16:9/w_1920,c_limit/depression-gq-7aug18_istock_b.jpg',
-    author: 'Ali',
-    authorDetails: 'whatever whatever',
-    authorImg:
-      'https://i2-prod.chroniclelive.co.uk/incoming/article12051405.ece/ALTERNATES/s615b/LJ-Ross-interview.jpg',
-    text:
-      'Depression is a common mental disorder affecting more than 264 million people worldwide. It is characterized by persistent sadness and a lack of interest or pleasure in previously rewarding or enjoyable activities.  It can also disturb sleep and appetite; tiredness and poor concentration are common. Depression is a leading cause of disability around the world and contributes greatly to the global burden of disease. The effects of depression can be long-lasting or recurrent and can dramatically affect a person’s ability to function and live a rewarding life. The causes of depression include complex interactions between social, psychological and biological factors. Life events such as childhood adversity, loss and unemployment contribute to and may catalyse the development of depression. Psychological and pharmacological treatments exist for moderate and severe depression. However, in low- and middle-income countries, treatment and support services for depression are often absent or underdeveloped. An estimated 76–85% of people suffering from mental disorders in these countries lack access to the treatment they need. ',
-  },
-];
-
-export function getArticles() {
-  return articles.map((a) => a);
+async function fetchArticles() {
+  const ftch = await fetch('http://localhost:5000/api/getList');
+  const response = await ftch.json();
+  // eslint-disable-next-line no-return-await
+  return await response;
 }
+async function getArticles() {
+  return fetchArticles();
+}
+
+const articles = getArticles();
+export default articles;
