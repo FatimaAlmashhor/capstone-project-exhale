8439de1c58a83c212734281f66c2a8e9a0424acb
Fatima Amin <fatima.amin4official@gmail.com>
2020-11-10 18:18:23 +0300

    Test: Fix articles Test: Fix Footer

--- src/__test__/Articles.test.js
+++ src/__test__/Articles.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'react-router-dom';
 import renderer from 'react-test-renderer';
 import {BrowserRouter as  Router} from 'react-router-dom'
-import Articles from '../containers/Articles';
+import Articles   from '../containers/Articles';
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Router><Articles /></Router> )
+    .create(<Router><Articles /></Router>)
     .toJSON();
   expect(tree).toMatchSnapshot();
-});
+})
