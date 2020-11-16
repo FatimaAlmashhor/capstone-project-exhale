c5f1e22f2fb77fedec48895bfcdbff7c0d626d81
pepeep <noha.pepeep1@gmail.com>
2020-11-16 17:43:16 +0300

    feat : add new component Cursol for Carousel
    solve test

--- src/__test__/Cursol.test.js
+++ src/__test__/Cursol.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Cursol from '../components/Cursol/Cursol';
+import Carousel from '@brainhubeu/react-carousel';
+
 it('renders correctly', () => {
-  const tree = renderer.create(<Cursol />).toJSON();
+  const tree = renderer.create(<Cursol ><Carousel /> </Cursol>).toJSON();
   expect(tree).toMatchSnapshot();
 });
