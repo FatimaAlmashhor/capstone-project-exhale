b2df5f11120f65a0c5c738d933c6538ce5ca43c2
Fatima Amin <fatima.amin4official@gmail.com>
2020-11-17 23:07:03 +0300

    Style : Change  contact head

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -4,7 +4,26 @@ import ContactUsForm from '../../components/ContactUsForm';
 const Contact = () => {
   return (
     <div>
-      <ContactUsForm />
+      <div
+        className="Header flex-grow  w-full h-64 top-0 py-64 "
+        style={{ backgroundColor: '#ADCCD2' }}
+      >
+        <svg
+          className="absolute buttom-0 h-64 w-full"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="white" points="0,100 100,0 100,100" />
+        </svg>
+      </div>
+      <div
+        className="absolute h-full mx-auto right-0 left-0  "
+        style={{ top: '80px' }}
+      >
+        <ContactUsForm />
+      </div>
+      <div className="block h-full py-64 " />
     </div>
   );
 };
