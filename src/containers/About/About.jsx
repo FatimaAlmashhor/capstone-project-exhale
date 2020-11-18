4be5c3abf17974dfb752d3717c1c34b81bf74e74
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-11-18 08:02:58 +0300

    Fix: add Lottie to values section.

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import Lottie from 'react-lottie';
 import TeamCard from '../../components/teamCard';
 import { ReactComponent as WhatDo } from '../../asset/svg/whatdo3.svg';
-import { ReactComponent as Respect } from '../../asset/svg/respect.svg';
-import { ReactComponent as Compassion } from '../../asset/svg/compassion.svg';
-import { ReactComponent as Positivity } from '../../asset/svg/positivity.svg';
+import respect from '../../Lottie/respect.json';
+import compassion from '../../Lottie/compassion.json';
+import positivity from '../../Lottie/positivity.json';
 
 const About = () => {
   const { t } = useTranslation();
-
+  const [hoverLottie, setHoverLottie] = useState(false);
+  const defaultOptions = (data) => {
+    return {
+      loop: false,
+      autoplay: hoverLottie,
+      animationData: data,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+      },
+    };
+  };
   return (
     <div>
       <section className="relative pt-10 ">
@@ -74,15 +85,39 @@ const About = () => {
             <h2 className="text-4xl font-semibold text-blue-700">Our Values</h2>
             <div className="justify-between mt-10 -mb-4 md:flex md:flex-wrap">
               <div className="inline-flex items-center justify-center w-1/3 p-2">
-                <Respect />
+                <div className="relative w-64 h-64 mx-auto overflow-hidden">
+                  <Lottie
+                    isClickToPauseDisabled
+                    options={defaultOptions(respect)}
+                    height="auto"
+                    width="100%"
+                    onMouseOver={() => setHoverLottie(true)}
+                  />
+                </div>
                 <p className="text-xl font-semibold text-green-600">Respect</p>
               </div>
               <div className="inline-flex items-center justify-center w-1/3">
-                <Compassion />
+                <div className="relative w-64 h-64 mx-auto overflow-hidden">
+                  <Lottie
+                    isClickToPauseDisabled
+                    options={defaultOptions(compassion)}
+                    height="auto"
+                    width="100%"
+                    onMouseOver={() => setHoverLottie(true)}
+                  />
+                </div>
                 <p className="text-xl font-semibold text-green-600">Compassion</p>
               </div>
               <div className="inline-flex items-center justify-center w-1/3">
-                <Positivity />
+                <div className="relative w-64 h-64 mx-auto overflow-hidden">
+                  <Lottie
+                    isClickToPauseDisabled
+                    options={defaultOptions(positivity)}
+                    height="auto"
+                    width="100%"
+                    onMouseOver={() => setHoverLottie(true)}
+                  />
+                </div>
                 <p className="text-xl font-semibold text-green-600">Positivity</p>
               </div>
             </div>
