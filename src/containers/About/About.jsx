33ad31dc7ca93218a6b98d31bd76d1fa44533b41
pepeep <noha.pepeep1@gmail.com>
2020-11-19 02:17:06 +0300

    feat : fixing about us
    adding the team photos

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import TeamCard from '../../components/teamCard';
+import Card from '../../components/common/Card/Card';
 import { ReactComponent as WhatDo } from '../../asset/svg/whatdo3.svg';
+import { members } from '../../services/fakeTeamInfo';
 
 const About = () => {
   const { t } = useTranslation();
-
+  // const svg1 = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: Svg1,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice"
+  //   }
+  // };
+  // const svg2 = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: Svg2,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice"
+  //   }
+  // };
   return (
     <div>
-      <section className="mt-10 pt-10 relative bg-gray-200">
+      <section className=" pt-10 relative ">
         <div className="justify-center text-center flex flex-wrap py-8">
           <div className="w-full md:w-6/12 px-12 md:px-4">
             <h2 className="font-semibold text-4xl text-blue-700">{t('aboutus')}</h2>
@@ -25,9 +42,6 @@ const About = () => {
             </div>
             <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
               <div className="md:pr-4">
-                <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-rocket text-xl" />
-                </div>
                 <h3 className="text-3xl font-semibold text-green-500">
                   {t('mission')}
                 </h3>
@@ -47,12 +61,10 @@ const About = () => {
         height={400}
         width={400}
       /> */}
+              {/* <FishGirl /> */}
             </div>
             <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
               <div className="md:pr-4">
-                <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-chart-line text-xl" />
-                </div>
                 <h3 className="text-3xl font-semibold text-green-500">
                   {t('ourgoals')}
                 </h3>
@@ -66,38 +78,27 @@ const About = () => {
       </section>
 
       <section className="mt-40 pb-20 py-10 relative bg-gray-200">
-        <div
-          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute"
-          style={{ height: '80px', transform: 'translateZ(0px)' }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-gray-200 fill-current"
-              points="2560 0 2560 100 0 100"
-            />
-          </svg>
-        </div>
         <div className="justify-center text-center flex flex-wrap py-8">
-          <div className="w-full md:w-6/12 px-12 md:px-4">
+          <div className=" md:w-6/12">
             <h2 className="font-semibold text-4xl text-blue-700">
               {t('Our Values')}
             </h2>
-            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-600">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            </p>
+            <div className="justify-between mt-10 flex flex-wrap -mb-4">
+              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white ">
+                <i className="fas fa-chart-line text-xl" />
+              </div>
+              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white ">
+                <i className="fas fa-chart-line text-xl" />
+              </div>
+              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white  ">
+                <i className="fas fa-chart-line text-xl" />
+              </div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="mt-10 pb-20 py-10 relative ">
+      <section className="m-auto mt-10 pb-20 py-10 relative ">
         <div className="justify-center text-center flex flex-wrap py-8">
           <div className="w-full md:w-6/12 px-12 md:px-4">
             <h2 className="font-semibold text-4xl text-blue-700">{t('ourteam')}</h2>
@@ -107,25 +108,19 @@ const About = () => {
           </div>
         </div>
 
-        <div className="md:flex md:flex-wrap -mb-4">
-          <div className="mb-4  sm:w-1/1  md:w-1/2  lg:w-1/3">
-            <TeamCard />
-          </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
-            <TeamCard />
-          </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
-            <TeamCard />
-          </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
-            <TeamCard />
-          </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
-            <TeamCard />
-          </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
-            <TeamCard />
-          </div>
+        <div className="  md:flex md:flex-wrap -mb-4">
+          {members.map((doc) => (
+            <div className="p-8 mb-4  sm:w-1/1  md:w-1/2  lg:w-1/3">
+              <Card>
+                <TeamCard
+                  name={`${doc.id}. ${doc.name}`}
+                  major={`${doc.major}`}
+                  image={doc.img}
+                  email={`${doc.email}`}
+                />
+              </Card>
+            </div>
+          ))}
         </div>
       </section>
     </div>
