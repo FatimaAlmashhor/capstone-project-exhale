375014d815cb14c9a4f1f375f073330e827fa8a2
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-11-18 07:02:52 +0300

    Fix: add vlaues to about us page

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,29 +2,34 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import TeamCard from '../../components/teamCard';
 import { ReactComponent as WhatDo } from '../../asset/svg/whatdo3.svg';
+import { ReactComponent as Respect } from '../../asset/svg/respect.svg';
+import { ReactComponent as Compassion } from '../../asset/svg/compassion.svg';
+import { ReactComponent as Positivity } from '../../asset/svg/positivity.svg';
 
 const About = () => {
   const { t } = useTranslation();
 
   return (
     <div>
-      <section className=" pt-10 relative ">
-        <div className="justify-center text-center flex flex-wrap py-8">
-          <div className="w-full md:w-6/12 px-12 md:px-4">
-            <h2 className="font-semibold text-4xl text-blue-700">About Us</h2>
-            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-600">
+      <section className="relative pt-10 ">
+        <div className="flex flex-wrap justify-center py-8 text-center">
+          <div className="w-full px-12 md:w-6/12 md:px-4">
+            <h2 className="mt-32 text-4xl text-blue-700 m-tfont-semibold">
+              About Us
+            </h2>
+            <p className="mt-4 mb-4 text-lg leading-relaxed text-gray-600">
               We wont give up until everyone experiencing a mental health problem
               gets support and respect.
             </p>
           </div>
         </div>
 
-        <div className="container mx-auto px-4 py-8">
-          <div className="items-center flex flex-col md:flex-row">
-            <div className="w-full md:w-6/12 mr-auto px-4 md:pt-0 my-0">
+        <div className="container px-4 py-8 mx-auto">
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="w-full px-4 my-0 mr-auto md:w-6/12 md:pt-0">
               <WhatDo />
             </div>
-            <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <div className="w-full px-4 ml-auto mr-auto md:w-5/12">
               <div className="md:pr-4">
                 <h3 className="text-3xl font-semibold text-green-500">What we do</h3>
                 <p className="mt-4 text-lg leading-relaxed text-gray-600">
@@ -37,16 +42,16 @@ const About = () => {
           </div>
         </div>
 
-        <div className="container mx-auto px-4 py-8">
-          <div className="items-center flex flex-col md:flex-row-reverse">
-            <div className="w-full md:w-4/12 mr-auto px-4 md:pt-0 my-0">
+        <div className="container px-4 py-8 mx-auto">
+          <div className="flex flex-col items-center md:flex-row-reverse">
+            <div className="w-full px-4 my-0 mr-auto md:w-4/12 md:pt-0">
               {/* <Lottie 
     options={svg2}
       height={400}
       width={400}
     /> */}
             </div>
-            <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <div className="w-full px-4 ml-auto mr-auto md:w-5/12">
               <div className="md:pr-4">
                 <h3 className="text-3xl font-semibold text-green-500">
                   What we aim
@@ -63,52 +68,55 @@ const About = () => {
         </div>
       </section>
 
-      <section className="mt-40 pb-20 py-10 relative bg-gray-200">
-        <div className="justify-center text-center flex flex-wrap py-8">
-          <div className="w-full md:w-6/12 px-12 md:px-4">
-            <h2 className="font-semibold text-4xl text-blue-700">Our Values</h2>
-            <div className="justify-between mt-10 md:flex md:flex-wrap -mb-4">
-              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-chart-line text-xl" />
+      <section className="relative py-10 pb-20 mt-40 bg-gray-200">
+        <div className="flex flex-wrap justify-center py-8 text-center">
+          <div className="w-full px-12 md:w-6/12 md:px-4">
+            <h2 className="text-4xl font-semibold text-blue-700">Our Values</h2>
+            <div className="justify-between mt-10 -mb-4 md:flex md:flex-wrap">
+              <div className="inline-flex items-center justify-center w-1/3 p-2">
+                <Respect />
+                <p className="text-xl font-semibold text-green-600">Respect</p>
               </div>
-              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-chart-line text-xl" />
+              <div className="inline-flex items-center justify-center w-1/3">
+                <Compassion />
+                <p className="text-xl font-semibold text-green-600">Compassion</p>
               </div>
-              <div className="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-chart-line text-xl" />
+              <div className="inline-flex items-center justify-center w-1/3">
+                <Positivity />
+                <p className="text-xl font-semibold text-green-600">Positivity</p>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="mt-10 pb-20 py-10 relative ">
-        <div className="justify-center text-center flex flex-wrap py-8">
-          <div className="w-full md:w-6/12 px-12 md:px-4">
-            <h2 className="font-semibold text-4xl text-blue-700">{t('ourteam')}</h2>
-            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-600">
+      <section className="relative py-10 pb-20 mt-10 ">
+        <div className="flex flex-wrap justify-center py-8 text-center">
+          <div className="w-full px-12 md:w-6/12 md:px-4">
+            <h2 className="text-4xl font-semibold text-blue-700">{t('ourteam')}</h2>
+            <p className="mt-4 mb-4 text-lg leading-relaxed text-gray-600">
               {t('detailteam')}
             </p>
           </div>
         </div>
 
-        <div className="md:flex md:flex-wrap -mb-4">
-          <div className="mb-4  sm:w-1/1  md:w-1/2  lg:w-1/3">
+        <div className="-mb-4 md:flex md:flex-wrap">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
-          <div className="mb-4  sm:w-1/1  md:w-1/2 lg:w-1/3">
+          <div className="mb-4 sm:w-1/1 md:w-1/2 lg:w-1/3">
             <TeamCard />
           </div>
         </div>
