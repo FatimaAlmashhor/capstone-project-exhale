f61718f3f4060dda89bbe0f533fdfa1496b3a428
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-11-24 03:42:21 +0300

    Fix: add doctors imgs to asset folder.

--- src/services/fakeDoctorsServices.js
+++ src/services/fakeDoctorsServices.js
@@ -1,9 +1,16 @@
 import i18n from '../i18next';
+import DrNabeel from '../asset/images/DoctorsPhotos/Dr. Nabil Nouman.jpg';
+import DrKulaidi from '../asset/images/DoctorsPhotos/Dr. Mohammed AlKolaidi.jpg';
+import DrBelgees from '../asset/images/DoctorsPhotos/Dr. Belqees.jpg';
+import DrMahfoodh from '../asset/images/DoctorsPhotos/Dr. Mahfoodh.jpg';
+import DrSaif from '../asset/images/DoctorsPhotos/Dr. Saif AlDeen.jpg';
+import MaleDefault from '../asset/images/Male Doctor - Default img.png';
+import FemaleDefault from '../asset/images/Female Doctor - Default img.png';
 
 export const doctors = [
   {
     id: 1,
-    image: 'http://www.sghsanaa.com/imgs/doctors/nabil_1.jpg',
+    image: DrNabeel,
     name: i18n.t('DrNabeel'),
     specialty: i18n.t('Nabeelspecialty'),
     workplace: i18n.t('Nabeelworkplace'),
@@ -13,7 +20,7 @@ export const doctors = [
   },
   {
     id: 2,
-    image: 'http://ye-mj.net/userimages/2019/06/1560527133blobid0.jpg',
+    image: DrKulaidi,
     name: i18n.t('DrKulaidi'),
     specialty: i18n.t('Kulaidispecialty'),
     workplace: i18n.t('Kulaidiworkplace'),
@@ -23,7 +30,7 @@ export const doctors = [
   },
   {
     id: 3,
-    image: 'http://fcdf-ye.org/Uploads/637016382831266987.jpg',
+    image: DrBelgees,
     name: i18n.t('DrBelgees'),
     specialty: i18n.t('BMSpecialty'),
     workplace: i18n.t('Belgeesworkplace'),
@@ -33,8 +40,7 @@ export const doctors = [
   },
   {
     id: 4,
-    image:
-      'https://scontent.fsah1-1.fna.fbcdn.net/v/t1.0-9/52699376_2246048215615990_963345539401777152_n.jpg?_nc_cat=110&ccb=2&_nc_sid=85a577&_nc_ohc=06Mm3DZq_HEAX-OjfVA&_nc_ht=scontent.fsah1-1.fna&oh=c07b0d89314177dcbc6a6afae001a634&oe=5FDA6EB6',
+    image: DrMahfoodh,
     name: i18n.t('DrMahfoodh'),
     specialty: i18n.t('BMSpecialty'),
     workplace: i18n.t('Mahfoodworkplace'),
@@ -44,7 +50,7 @@ export const doctors = [
   },
   {
     id: 5,
-    image: 'http://fcdf-ye.org/Uploads/636528748345104319.jpg',
+    image: DrSaif,
     name: i18n.t('DrSaif'),
     specialty: i18n.t('BMSpecialty'),
     workplace: i18n.t('Belgeesworkplace'),
@@ -54,8 +60,7 @@ export const doctors = [
   },
   {
     id: 6,
-    image:
-      'https://www.shareicon.net/data/512x512/2016/08/18/813849_people_512x512.png',
+    image: FemaleDefault,
     name: i18n.t('DrSamah'),
     specialty: i18n.t('SamahSpecialty'),
     workplace: i18n.t('Samahworkplace'),
@@ -65,8 +70,7 @@ export const doctors = [
   },
   {
     id: 7,
-    image:
-      'https://www.shareicon.net/data/512x512/2016/08/18/813844_people_512x512.png',
+    image: MaleDefault,
     name: i18n.t('DrAlsharabi'),
     specialty: i18n.t('BMSpecialty'),
     workplace: i18n.t('Alsharabiworkplace'),
@@ -76,8 +80,7 @@ export const doctors = [
   },
   {
     id: 8,
-    image:
-      'https://www.shareicon.net/data/512x512/2016/08/18/813844_people_512x512.png',
+    image: MaleDefault,
     name: i18n.t('DrAbdulqader'),
     specialty: i18n.t('BMSpecialty'),
     workplace: i18n.t('Abdulqaderworkplac'),
